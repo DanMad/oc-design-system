43605c061129b085160be168cdacab98aac2d047
Daniel Maddison <dmaddison@icloud.com>
2021-10-08 20:50:36 +1100

    Updated Webpack config

--- webpack.config.babel.ts
+++ webpack.config.babel.ts
@@ -2,7 +2,7 @@
 import tokenImporter from 'node-sass-token-importer';
 import babel from './babel.config.json';
 
-const webpackConfig = {
+const config = {
   devtool: 'source-map',
   module: {
     rules: [
@@ -10,7 +10,10 @@ const webpackConfig = {
         exclude: /node_modules/,
         test: /\.(scss)$/,
         use: [
-          'style-loader',
+          {
+            loader: 'style-loader',
+            options: { injectType: 'singletonStyleTag' },
+          },
           'css-loader',
           'postcss-loader',
           {
@@ -59,4 +62,4 @@ const webpackConfig = {
   },
 };
 
-export { webpackConfig as default };
+export { config as default };
